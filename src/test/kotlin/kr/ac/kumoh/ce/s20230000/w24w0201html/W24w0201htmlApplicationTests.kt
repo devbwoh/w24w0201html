f6b446f29c00f7404e555ca03534bfd71096da26
bwoh <bwoh@outlook.kr>
2024-09-10 16:30:16 +0900

    🎉 HTML 시작

---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20230000/w24w0201html/W24w0201htmlApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20230000.w24w0201html
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0201htmlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
